3869ea1ba8165084624fe2e23d5bf281aed11ddb
jefferson1104 <jefferson1104junior@gmail.com>
2022-07-09 23:19:38 -0300

    test UserList component

--- src/components/UserList/UserList.test.tsx
+++ src/components/UserList/UserList.test.tsx
@@ -12,4 +12,18 @@ describe('UserList component', () => {
     expect(screen.getByText('Member')).toBeInTheDocument()
     expect(screen.getByText('Points')).toBeInTheDocument()
   })
+
+  it('should render list with users', () => {
+    render(<UserList users={usersMock} />)
+
+    const userName = screen.getByText('Cameron Williamson')
+    const userAvatar = screen.getByRole('img', {
+      name: /cameron williamson/i
+    })
+    const userPoints = screen.getByText(/541/i)
+
+    expect(userName).toBeInTheDocument()
+    expect(userAvatar).toBeInTheDocument()
+    expect(userPoints).toBeInTheDocument()
+  })
 })
